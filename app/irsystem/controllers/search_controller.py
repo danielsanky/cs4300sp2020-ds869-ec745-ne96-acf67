1f8c0e78e0bc24de5ca736feae117a20797cc92a
ne96 <ne96@cornell.edu>
2020-04-25 15:16:43 -0400

    implementing music

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -18,6 +18,11 @@ def back():
 def recommender():
     podcast_query = request.args.getlist("all-interests")
     movie_query = request.args.getlist("all-movies")
+    music_query = request.args.getlist("all-music")
+    if music_query!=[]:
+        song=recs(genre_to_music, music_query)
+    else:
+        song=["We can't recommend you a song, since you don't know what music you like!"]
     if podcast_query!=[]:
         podcast=recs(final_dict, podcast_query)
     else:
@@ -34,26 +39,17 @@ cols = pd.read_csv("young-people-survey/columns.csv")
 resp = pd.read_csv("young-people-survey/responses.csv")
 podcasts=pd.read_csv("young-people-survey/df_popular_podcasts.csv")
 movies=pd.read_csv("young-people-survey/MovieGenre.csv")
+music=pd.read_csv("young-people-survey/SpotifyFeatures.csv")
 genre_IDs=[['1311', 'News & Politics'], ['26', 'Podcasts'], ['1479', 'Social Sciences'], ['1315', 'Science & Medicine'], ['1324', 'Society & Culture'], ['1302', 'Personal Journals'], ['1469', 'Language Courses'], ['1304', 'Education'], ['1320', 'Places & Travel'], ['1416', 'Higher Education'], ['1465', 'Professional'], ['1316', 'Sports & Recreation'], ['1303', 'Comedy'], ['1305', 'Kids & Family'], ['1439', 'Christianity'], ['1314', 'Religion & Spirituality'], ['1444', 'Spirituality'], ['1309', 'TV & Film'], ['1462', 'History'], ['1310', 'Music'], ['1478', 'Medicine'], ['1321', 'Business'], ['1412', 'Investing'], ['1420', 'Self-Help'], ['1307', 'Health'], ['1481', 'Alternative Health'], ['1417', 'Fitness & Nutrition'], ['1467', 'Amateur'], ['1480', 'Software How-To'], ['1318', 'Technology'], ['1448', 'Tech News'], ['1456', 'Outdoor'], ['1477', 'Natural Sciences'], ['1301', 'Arts'], ['1454', 'Automotive'], ['1323', 'Games & Hobbies'], ['1438', 'Buddhism'], ['1443', 'Philosophy'], ['1401', 'Literature'], ['1402', 'Design'], ['1410', 'Careers'], ['1470', 'Training'], ['1413', 'Management & Marketing'], ['1306', 'Food'], ['1406', 'Visual Arts'], ['1446', 'Gadgets'], ['1468', 'Educational Technology'], ['1405', 'Performing Arts'], ['1460', 'Hobbies'], ['1471', 'Business News'], ['1404', 'Video Games'], ['1450', 'Podcasting'], ['1473', 'National'], ['1325', 'Government & Organizations'], ['1461', 'Other Games'], ['1466', 'College & High School'], ['1459', 'Fashion & Beauty'], ['1476', 'Non-Profit'], ['1415', 'K-12'], ['1455', 'Aviation'], ['1464', 'Other'], ['1421', 'Sexuality'], ['1472', 'Shopping'], ['1475', 'Local'], ['1441', 'Judaism'], ['1440', 'Islam'], ['1474', 'Regional'], ['1463', 'Hinduism']]
 
 #dependent vbs
-music=cols[:19]
+music_cols=cols[:19]
 movie_cols=cols[19:31]
 hobbies=cols[31:63]
 
 #independent vbs
 personality=cols[76:133]
 demographics=cols[140:147]
-
-#Prep the podcasts dataset for the function 
-
-#Get rid of columns we won't use
-#podcasts=podcasts.drop('Artwork', axis=1)
-#podcasts=podcasts.drop('Episode Count', axis=1)
-#podcasts=podcasts.drop('Episode Durations', axis=1)
-#podcasts=podcasts.drop('iTunes URL', axis=1)
-#podcasts=podcasts.drop('Feed URL', axis=1)
-podcasts.head()
 
 # Better organize the id vs name of genre
 dict_pod={int(id[0]):id[1] for id in genre_IDs }
@@ -69,8 +65,6 @@ for item in podcasts.index:
 
 final_dict={list(dict_pod.values())[p]:list(genre_to_podcast.values())[p] for p in range(len(genre_to_podcast.keys()))}
 
-movies=movies[:10000]
-
 genre_to_movie={}
 for i in range(len(movies)):
     line=str(movies["Genre"][i])
@@ -80,6 +74,13 @@ for i in range(len(movies)):
         else: 
             genre_to_movie[genre]=[movies["Title"][i]]
 
+genre_to_music={}
+for i in range(len(music)):
+    genre=music['genre'][i]
+    if genre in genre_to_music:
+            genre_to_music[genre].append(music['track_name'][i])
+    else:
+            genre_to_music[genre]=[(music['track_name'][i], music['artist_name'][i])]
 
 def recs(genre_dict, query):
     """Returns a list of podcasts based on interests user clicked in form
